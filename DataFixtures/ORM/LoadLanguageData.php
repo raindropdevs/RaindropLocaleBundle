bd4b8b0d60fcedaaa1d31d2a573fd3bd5d4540fc
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-04-03 12:37:04 +0200

    add language fixtures

---
+++ DataFixtures/ORM/LoadLanguageData.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Raindrop\LocaleBundle\DataFixtures\ORM;
+
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\Yaml\Yaml;
+use Raindrop\LocaleBundle\Entity\Language;
+
+/**
+ * Language Data Fixtures
+ */
+class LoadLanguageData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * @{inheritDoc}
+     */    
+    public function load(ObjectManager $manager)
+    {
+        $languages = Yaml::parse(__DIR__ . "/../../Resources/data/language.yml");
+
+        foreach ($languages as $code => $name) {
+            // persist all countries
+            $language = new Language();
+            $language->setCode($code);
+            $language->setName($name);
+            $manager->persist($language);
+        }
+        
+        $manager->flush();
+    }
+
+    /**
+     * @{inheritDoc}
+     */        
+    public function getOrder()
+    {
+        return 2;
+    }    
+}
