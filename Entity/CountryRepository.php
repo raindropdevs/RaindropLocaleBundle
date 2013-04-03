4fd308d05a12c8b1592a4dc408dffeb8d09799f6
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-04-03 14:57:25 +0200

    add country and language repository

---
+++ Entity/CountryRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Raindrop\LocaleBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Country Repository
+ */
+class CountryRepository extends EntityRepository
+{
+    //put your code here
+}
---
+++ Entity/LanguageRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Raindrop\LocaleBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Language Repository
+ */
+class LanguageRepository extends EntityRepository
+{
+    //put your code here
+}
