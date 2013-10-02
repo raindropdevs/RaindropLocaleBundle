542514660b2172b882fd54f226f2eeb6370249d7
Edoardo Rivello <edoardo.rivello@gmail.com>
2013-10-02 14:39:47 +0200

    fix (for the moment) the international countries admin

--- Admin/Entity/InternationalAdmin.php
+++ Admin/Entity/InternationalAdmin.php
@@ -28,40 +28,9 @@ class InternationalAdmin extends Admin
      */
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $entity = $this->getSubject();
-        $id = $entity->getId();
-
-        if (is_null($id)) {
-            $language = $this->modelManager
-                    ->getEntityManager('Raindrop\LocaleBundle\Entity\Language')
-                    ->getRepository('Raindrop\LocaleBundle\Entity\Language')
-                    ->createQueryBuilder('l')
-                    ->where('l.id NOT IN (SELECT il.id FROM RaindropLocaleBundle:International i LEFT JOIN i.language il)');
-
-            $country = $this->modelManager
-                    ->getEntityManager('Raindrop\LocaleBundle\Entity\Country')
-                    ->getRepository('Raindrop\LocaleBundle\Entity\Country')
-                    ->createQueryBuilder('c')
-                    ->where('c.id NOT IN (SELECT ic.id FROM RaindropLocaleBundle:International i LEFT JOIN i.countries ic)');
-        } else {
-            $language = $this->modelManager
-                    ->getEntityManager('Raindrop\LocaleBundle\Entity\Language')
-                    ->getRepository('Raindrop\LocaleBundle\Entity\Language')
-                    ->createQueryBuilder('l')
-                    ->where('l.id NOT IN (SELECT il.id FROM RaindropLocaleBundle:International i LEFT JOIN i.language il WHERE i.id <> :id)')
-                    ->setParameter('id', $id);
-
-            $country = $this->modelManager
-                    ->getEntityManager('Raindrop\LocaleBundle\Entity\Country')
-                    ->getRepository('Raindrop\LocaleBundle\Entity\Country')
-                    ->createQueryBuilder('c')
-                    ->where('c.id NOT IN (SELECT ic.id FROM RaindropLocaleBundle:International i LEFT JOIN i.countries ic WHERE i.id <> :id)')
-                    ->setParameter('id', $id);
-        }
-
         $formMapper
-            ->add('language', null, array('required' => true, 'query_builder' => $language))
-            ->add('countries', null, array('required' => false, 'query_builder' => $country));
+            ->add('language', null, array('required' => true))
+            ->add('countries', null, array('required' => false));
     }
 
     /**
